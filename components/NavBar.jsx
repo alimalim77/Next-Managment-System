494d43484ae25e89b1276feac26a6cbe2120bc26
alimalim77 <alimalim77@gmail.com>
2023-06-28 17:37:23 +0530

    Commit: Next App

---
+++ components/NavBar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
+      <Link className="text-white font-bold" href={"/"}>
+        East
+      </Link>
+      <Link className="bg-white p-2" href={"/addTopic"}>
+        West
+      </Link>
+    </nav>
+  );
+}
