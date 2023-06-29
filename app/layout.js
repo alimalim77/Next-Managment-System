5742fba7d22e5f85ac7cf9bd863285f0472fc497
alimalim77 <alimalim77@gmail.com>
2023-06-29 16:48:41 +0530

    Commit: Update

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="max-w-3xl mx-auto p-4">
-          <Login />
+          {/* <Login /> */}
           <Navbar />
           <div className="mt-8">{children}</div>
         </div>
